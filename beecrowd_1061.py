6aaebf45383759ef6d5c9ef7f7c8cf9bbe1653de
André Bezerra <andrefrbezerra@gmail.com>
2024-01-09 22:01:56 -0300

    Update function configuration Beecrowd 1061 Python 3.11

--- beecrowd_1061.py
+++ beecrowd_1061.py
@@ -3,17 +3,16 @@ def data():
     h,m,s = map(int,input().split(":"))
     return [int(d),h,m,s]
 
+def analyse(a,b,x):
+	    return (a+x, b-1) if a < 0 else (a,b)
+	
+score = (24,60,60)
 x, y = data(), data()
+
 for i in range(len(y)): 
-    y[i] -= x[i]
-
-if(y[3]<0):
-	y[3], y[2] = y[3]+60, y[2]-1
-
-if(y[2]<0):
-    y[2], y[1] = y[2]+60, y[1]-1
-
-if(y[1]<0):
-    y[1], y[0] = y[1]+24, y[0]-1
+    y[i] = y[i] - x[i]
+    
+for i in range(len(y)-1,0,-1):
+    y[i],y[i-1] = analyse(y[i],y[i-1], score[i-1])
 
 print("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)" %(y[0],y[1],y[2],y[3]))
