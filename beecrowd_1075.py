d64f391e890caf607e1a42559f6fc58e1096cc21
André Bezerra <andrefrbezerra@gmail.com>
2024-01-14 15:50:37 -0300

    FIX: Beecrowd 1075 in Python 3.11 - wrong function

--- beecrowd_1075.py
+++ beecrowd_1075.py
@@ -1,5 +1,4 @@
-ans = ("NULL","EVEN","ODD","POSITIVE","NEGATIVE")
-for i in range(int(input())):
-    x = int(input())
-    x = f'{ans[x]}' if x == 0 else f'{ans[1 + abs(x%2)]} {ans[3 + (x < 0)]}'
-    print(x)
+X = int(input())
+for i in range(10000):
+    if (i%X == 2):
+        print("%d" %i)
