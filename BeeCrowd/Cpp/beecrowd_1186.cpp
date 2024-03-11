4caf7fde311bfe99f6a6e3861bc1ee9acf0d9a41
w1s4sh1i <andrefrbezerra@gmail.com>
2024-03-10 23:04:49 -0300

    message

---
+++ BeeCrowd/Cpp/beecrowd_1186.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  double n, add = 0, count = 0;
+  int lin, col;
+  char tag;
+
+  scanf("%c", &tag);
+
+  for (lin = 0; lin < 12; ++lin) {
+    for (col = 0; col < 12; ++col) {
+      scanf("%lf", &n);
+      if (col > 11 - lin) {
+        add += n;
+        count += 1;
+      }
+    }
+  }
+
+  printf("%.1lf\n", (tag == 'M') ? add / count : add);
+
+  return 0;
+}
---
+++ BeeCrowd/Python/beecrowd_1186.py
@@ -0,0 +1,11 @@
+add, count = 0, 0
+tag = input()
+
+for lin in range(12):
+    for col in range(12):
+        n = float(input())
+        if (col > 11-lin):
+            add += n
+            count += 1
+
+print('%.1f' %(add/count if tag == 'M' else add) )
